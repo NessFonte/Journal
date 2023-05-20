4802ea14258130a6bfc70c90098f24f05a766bdb
neisser <neisser2909@gmail.com>
2023-05-20 17:44:33 -0400

    Creado componente NoEntrySelected con button para añadir contenido

---
+++ src/modules/daybook/components/Fab.vue
@@ -0,0 +1,21 @@
+<template>
+  <button class="btn btn-primary">
+    <i class="fa fa-2px fa-plus"></i>
+  </button>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+button {
+  bottom: 20px;
+  height: 50px;
+  position: fixed;
+  right: 20px;
+  width: 50px;
+}
+</style>
