7e8c73c94ab10744122ecb2a6fd60b016ed6a68a
neisser <neisser2909@gmail.com>
2023-05-20 23:45:11 -0400

    Creado filtro de búsqueda en el EntryList

--- src/modules/daybook/store/journal/getters.js
+++ src/modules/daybook/store/journal/getters.js
@@ -1,5 +1,7 @@
-export const getEntryByTerm =  (state) => {
-    return state
+export const getEntriesByTerm =  (state) => (term = '') => {
+    if(term.length === 0) return state.entries
+
+    return state.entries.filter(entry => entry.text.toLowerCase().includes(term.toLowerCase()))
 }
 
 export const getEntryById =  (state) => {
--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -13,5 +13,11 @@ export default () => ({
             text: 'Officia in excepteur voluptate ut deserunt tempor proident labore duis mollit amet sunt officia. Culpa cupidatat labore aute et mollit sunt id ipsum magna sit nulla magna. Tempor occaecat officia est ad. Nisi sit laboris consectetur culpa culpa quis ut aliqua ea et. Non commodo consequat velit est velit. Eu laboris cillum fugiat cupidatat nulla pariatur sint nostrud nostrud ipsum.',
             picture: null,
         },
+        {
+            id: new Date().getTime() + 3000,
+            date: new Date().toDateString(), 
+            text: 'Nulla consectetur incididunt labore incididunt dolor est voluptate deserunt aliquip non. Nulla ea aliqua sit commodo adipisicing commodo cillum quis id id ipsum et ullamco. In esse veniam laborum eu non sunt cupidatat exercitation nisi culpa excepteur tempor consequat aliqua. Laboris reprehenderit culpa id mollit mollit id non magna adipisicing est pariatur culpa officia. Ipsum excepteur mollit laboris irure reprehenderit deserunt magna ad ea reprehenderit aute consectetur irure. Esse dolore mollit sint laborum enim reprehenderit exercitation qui amet sit. Veniam pariatur et aute non consectetur.',
+            picture: null,
+        },
     ]
 })
