aa8d7632f9a9ae026b1ec40831dfcf374aee8300
neisser <neisser2909@gmail.com>
2023-05-21 00:32:25 -0400

    Obtenidos los IDs de los componentes espec'ificos

--- src/modules/daybook/router/index.js
+++ src/modules/daybook/router/index.js
@@ -3,6 +3,12 @@ export default {
     component: () => import(/* webpackChunkName: "daybook" */ '@/modules/daybook/layouts/DayBookLayout.vue'),
     children: [
         {path: '', name: 'no-entry', component: () => import(/* webpackChunkName: "no-entry" */ '@/modules/daybook/views/NoEntrySelected.vue')},
-        {path: ':id', name: 'entry', component: () => import(/* webpackChunkName: "entry" */ '@/modules/daybook/views/EntryView.vue')},
+        {path: ':id', name: 'entry', component: () => import(/* webpackChunkName: "entry" */ '@/modules/daybook/views/EntryView.vue'),
+            props: (route) => {
+                return {
+                    id: route.params.id
+                }
+            }
+        },
     ]
 }
