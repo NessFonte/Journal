1bdd07813109b7f2b22858e8c98175a24d8f1384
neisser <neisser2909@gmail.com>
2023-05-21 11:28:40 -0400

    Creado un div de espera para cuando se carga una petición

--- src/modules/daybook/layouts/DayBookLayout.vue
+++ src/modules/daybook/layouts/DayBookLayout.vue
@@ -1,6 +1,16 @@
 <template>
     <Navbar />
-    <div class="d-flex">
+
+    <div class="row justify-content-md-center" v-if="isLoading">
+        <div class="col-3 alert-info text-center mt-5">
+            Espere por favor...
+            <h3 class="mt-2">
+                <i class="fa fa-spin fa-sync"></i>
+            </h3>
+        </div>
+    </div>
+
+    <div class="d-flex" v-else>
         <div class="col-4">
             <EntryList />
         </div>
@@ -12,7 +22,7 @@
 
 <script>
 import {defineAsyncComponent} from 'vue'
-import { mapActions } from 'vuex'
+import { mapActions, mapState } from 'vuex'
 
 export default {
     components: {
@@ -21,7 +31,11 @@ export default {
     },
     
     methods: {
-        ...mapActions('journal', ['loadEntries'])
+        ...mapActions('journal', ['loadEntries']),
+    },
+
+    computed: {
+        ...mapState('journal', ['isLoading'])
     },
 
     created() {
